dfeea3e21fb7f79e628e28b7d3f7c6689ccf86f7
Toufiqur0636 <bsse0636@iit.du.ac.bd>
2017-11-07 20:08:05 +0600

    Updated Reducer File for Assignment 4
    Reducer file for assignment 4 has been committed.

---
+++ assignment4/reducer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sys
+
+path_map = dict()
+
+for line in sys.stdin:
+	path = line.strip()
+	path_map[path] = path_map.get(path, 0) + 1
+
+popular = max(path_map, key=path_map.get)
+
+print path_map.get(popular, 0)
